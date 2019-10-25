1bf2b0e02aedfd1f6607c1d5237d212a29b9be0a
Josh <jcpsimmons@gmail.com>
2019-10-25 13:47:08 -0700

    cool - running!

--- constructors/htmlObj.py
+++ constructors/htmlObj.py
@@ -2,20 +2,17 @@ from datetime import datetime
 
 
 class HTMLObject:
-    def __init__(self):
-        self.type = ''
-        self.column = 0
-        self.row = 0
+    def __init__(self, row, column):
+        self.html = ""
+        self.column = column
+        self.row = row
         self.isVideo = False
         self.altText = ""
         self.isImage = False
         self.imageSrc = ""
 
-    def setLocation(self, column, row):
-        self.column = column
-        self.row = row
-
     def setImage(self, alt):
+        self.isImage = True
         self.alt = alt
         month = str(datetime.now().month)
         if len(month) < 2:
@@ -23,5 +20,20 @@ class HTMLObject:
         self.imageSrc = "/globalassets/images/lp/{}/{}/TK_IMAGE.JPG".format(
             month, str(datetime.now().year))
 
-    def videoTrue(self):
+    def videoTrue(self, alt):
         self.isVideo = True
+        self.alt = alt
+
+    def renderComponent(self):
+        # Image code
+        if self.isImage:
+            self.html = "<a href='/TK-LINK'><img src='{}' alt='{}' class='img-responsive'></a>".format(
+                self.imageSrc, self.alt)
+        # Video code - will need to insert modal later
+        if self.isVideo:
+            self.html = "<div class='videoModal'><div id='mobile_modal' data-toggle='modal' data-target='#modal-video_01' tabindex='0' data-theVideo='https://www.youtube.com/embed/ybo0J2A5umk'><img class='img-responsive video-thumb' src='/TK-VIDEO-THUMB.JPG' alt='{}'></div></div>".format(
+                self.alt)
+        # Nest it all in a div - No image or video? That's fine, it'll make a div with nothing in it!
+        self.html = "<div class='{}{}'>{}</div>".format(
+            str(self.row), str(self.column), self.html)
+        return(self.html)
---
+++ main.py
@@ -0,0 +1,70 @@
+from constructors.htmlObj import HTMLObject
+from constructors import htmlSnippets
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+def locationParse(strLocation):
+    # Turns raw string input (CSV) into array for location of object in 2D array
+    insertLocation = strLocation.split(",")
+    insertLocation = list(map(lambda x: int(x)-1, insertLocation))
+    return(insertLocation)
+
+
+def mappingSequence():
+    # Creates HTMLObjects in a 2D array - each row has nested columns
+    vdom = []
+    hasVideo = False
+    rows = int(input('How many rows? '))
+    for row in range(rows):
+        colList = []
+        cols = int(input('How many columns in row {}? '.format(str(row+1))))
+        for col in range(cols):
+            colList.append(HTMLObject(row + 1, col + 1))
+        vdom.append(colList)
+
+    # IMAGES
+    numImages = int(input('How many cells contain images (0 for none)? '))
+    if numImages > 0:
+        for i in range(numImages):
+            insertLocation = input(
+                'What is the location for image {} in row/col format, e.g. 1,3? '.format(str(i+1)))
+            insertLocation = locationParse(insertLocation)
+            altText = input('Enter alt text for this image: ')
+            vdom[insertLocation[0]][insertLocation[1]].setImage(altText)
+
+    # VIDEO
+    isVideo = int(input('Is there a video (1 yes, 0 no)? '))
+    if isVideo:
+        insertLocation = input(
+            'What is the location of the video in row/col format, e.g. 1,3? ')
+        insertLocation = locationParse(insertLocation)
+        altText = input('Enter alt text for this image: ')
+        vdom[insertLocation[0]][insertLocation[1]].videoTrue(altText)
+
+    # Assemble HTML
+    finalHTML = ""
+    for row in range(len(vdom)):
+        columnCode = ""
+        for col in range(len(vdom[row])):
+            columnCode = columnCode + \
+                vdom[row][col].renderComponent()
+        rowCode = "<div class='container {}'>{}</div>".format(
+            "row{}".format(str(row+1)), columnCode)
+        finalHTML = finalHTML + rowCode
+
+    if hasVideo:
+        finalHTML = finalHTML + htmlSnippets.videoModal
+    finalHTML = htmlSnippets.css + "<div class='x'>" + finalHTML + "</div>"
+
+    print(finalHTML)
+    return(finalHTML)
+
+
+vdom = mappingSequence()
+# prettify the HTML
+vdom = BeautifulSoup(vdom)
+vdom = vdom.prettify()
+
+with open('x.html', 'w') as file:
+    file.write(vdom)
